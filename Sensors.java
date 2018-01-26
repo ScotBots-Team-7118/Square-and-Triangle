a39a5131ec5fff94564204d60dcb1ac967dc0257
unknown <nphilbrick26@gmail.com>
2018-01-26 15:49:30 -0800

    encoder commenting

--- Sensors.java
+++ Sensors.java
@@ -13,7 +13,7 @@ public class Sensors implements Pronstants {
 	public Sensors() {
 		//make up for encoder counts to rotations
 		long encR = encoderR/4096;
-		long encL = encoder L/4096;
+		long encL = encoderL/4096;
 		// Initializing encoders
 		encoderR = new Encoder(PORT_ENC_R1, PORT_ENC_R2, false); // Right encoder
 		encoderL = new Encoder(PORT_ENC_L1, PORT_ENC_L2, false); // Left encoder
